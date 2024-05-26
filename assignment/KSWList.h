56c8b671fb1d837a8dc205c6bf911f9c86f37f3c
김수원 <rla5233@naver.com>
2024-05-27 00:48:34 +0900

    김수원 : 업데이트

--- assignment/KSWList.h
+++ assignment/KSWList.h
@@ -8,17 +8,59 @@ namespace ksw
 	class list
 	{
 	private:
-		struct Node
+		class Node
 		{
+		public:
 			Node* Prev = nullptr;
 			Node* Next = nullptr;
-			Type Data;
+			Type Data = Type();
+		};
+
+	public:
+		class iterator
+		{
+			friend class list;
+		public:
+			iterator() {};
+			iterator(Node* _CurNode)
+				: CurNode(_CurNode)
+			{};
+
+			~iterator() {};
+
+			void operator++()
+			{
+				CurNode = CurNode->Next;
+			}
+
+			void operator--()
+			{
+				CurNode = CurNode->Prev;
+			}
+
+			Type& operator*()
+			{
+				return CurNode->Data;
+			}
+
+			bool operator!=(const iterator& _Other)
+			{
+				return CurNode != _Other.CurNode;
+			}
+
+			bool operator==(const iterator& _Other)
+			{
+				return CurNode == _Other.CurNode;
+			}
+
+		private:
+			Node* CurNode = nullptr;
 		};
 
 	public:
 		// constructor destructor
 		list() {};
-		~list() {};
+		~list();
 
 		// delete Function
 		list(const list& _Other) = delete;
@@ -27,18 +69,77 @@ namespace ksw
 		list& operator=(list&& _Other) noexcept = delete;
 
 	public:
-		void push_back(Type _Data);
+		Type front();
+		Type back();
 
+		void push_front(const Type& _Data);
+		void push_back(const Type& _Data);
+
+		iterator begin()
+		{
+			return iterator(Begin);
+		}
+
+		iterator end()
+		{
+			return iterator(End->Next);
+		}
+
+		inline size_t size()
+		{
+			return Size;
+		}
 
 	private:
 		Node* Begin = nullptr;
 		Node* End = nullptr;
+		size_t Size = 0;
 
 	};
 
+	// ±¸ÇöºÎ
+	template<typename Type>
+	inline ksw::list<Type>::~list()
+	{
+		Node* CurNode = Begin;
+		while (nullptr != CurNode)
+		{
+			Node* Next = CurNode->Next;
+			delete CurNode;
+			CurNode = Next;
+		}
+	}
 
 	template<typename Type>
-	inline void list<Type>::push_back(Type _Data)
+	inline Type list<Type>::front()
+	{
+		return Begin->Data;
+	}
+
+	template<typename Type>
+	inline Type list<Type>::back()
+	{
+		return End->Data;
+	}
+
+	template<typename Type>
+	inline void list<Type>::push_front(const Type& _Data)
+	{
+		Node* NewNode = new Node;
+		NewNode->Data = _Data;
+
+		if (nullptr != Begin)
+		{
+			NewNode->Next = Begin;
+			Begin->Prev = NewNode;
+		}
+
+		Begin = NewNode;
+		++Size;
+	}
+
+	template<typename Type>
+	inline void list<Type>::push_back(const Type& _Data)
 	{
 		Node* NewNode = new Node;
 		NewNode->Data = _Data;
@@ -49,12 +150,14 @@ namespace ksw
 		}
 		else
 		{
+			End->Next = NewNode;
 			NewNode->Prev = End;
 		}
 
 		End = NewNode;
+		++Size;
 	}
 
-
 }
+
 
