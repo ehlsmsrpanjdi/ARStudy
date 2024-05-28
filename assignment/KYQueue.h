037f26ea45b4cc90ee35dfd25fb7d104adc805bb
kky <kykim9050@naver.com>
2024-05-28 20:15:34 +0900

    김경윤 : KYQueue 클래스 헤더 추가

---
+++ assignment/KYQueue.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+class QNode;
+
+template<typename Type>
+class KYQueue
+{
+public:
+	
+
+
+protected:
+
+private:
+	QNode* FirstNode = nullptr;
+	QNode* LastNode = nullptr;
+
+private:
+	class QNode
+	{
+	public:
+
+	protected:
+
+	private:
+
+	};
+};
