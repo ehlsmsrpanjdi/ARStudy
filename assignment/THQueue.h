2f3851091e9aa923c186fa8acb86c4d3f78f7c8d
TaeHwan <alex23456@naver.com>
2024-05-28 21:45:03 +0900

    이태환: Queue 기본 구조 추가

---
+++ assignment/THQueue.h
@@ -0,0 +1,21 @@
+#pragma once
+
+template<typename T>
+class Queue
+{
+private:
+	class Node
+	{
+		Node* Back = nullptr;
+		Node* Front = nullptr;
+		T Value;
+	};
+
+public:
+
+protected:
+
+private:
+
+	
+};
