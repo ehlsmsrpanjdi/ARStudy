281210bb978545748c3bd02221f053d93831fbe8
usm <AssortRock0@AssortRock>
2024-05-31 13:50:42 +0900

    .

--- assignment/SMQueue.h
+++ assignment/SMQueue.h
@@ -1,87 +1,89 @@
 #pragma once
 
-template <typename Type>
-class Node {
-	template<typename Type> friend class SMQueue;
-public:
-	Node(Type _Type)
-		:Value(_Type)
+namespace SM {
+	template <typename Type>
+	class Node {
+		template<typename Type> friend class SMQueue;
+	public:
+		Node(Type _Type)
+			:Value(_Type)
+		{
+
+		}
+
+		~Node() {
+
+		}
+	private:
+		void NodeRelease() {
+			if (nullptr != Next) {
+				Next->NodeRelease();
+				delete Next;
+				Next = nullptr;
+			}
+		}
+		Node<Type>* Next = nullptr;
+		Type Value;
+	};
+
+	// 설명 :
+	template <typename Type>
+	class SMQueue
 	{
+	public:
+		// constrcuter destructer
+		SMQueue() {
 
-	}
-
-	~Node() {
-
-	}
-private:
-	void NodeRelease() {
-		if (nullptr != Next) {
-			Next->NodeRelease();
-			delete Next;
-			Next = nullptr;
 		}
-	}
-	Node<Type>* Next = nullptr;
-	Type Value;
-};
-
-// 설명 :
-template <typename Type>
-class SMQueue
-{
-public:
-	// constrcuter destructer
-	SMQueue() {
-
-	}
-	~SMQueue() {
-		if (Head != nullptr) {
-			Head->NodeRelease();
-			delete Head;
+		~SMQueue() {
+			if (Head != nullptr) {
+				Head->NodeRelease();
+				delete Head;
+			}
 		}
-	}
 
-	// delete Function
-	SMQueue(const SMQueue& _Other) = delete;
-	SMQueue(SMQueue&& _Other) noexcept = delete;
-	SMQueue& operator=(const SMQueue& _Other) = delete;
-	SMQueue& operator=(SMQueue&& _Other) noexcept = delete;
+		// delete Function
+		SMQueue(const SMQueue& _Other) = delete;
+		SMQueue(SMQueue&& _Other) noexcept = delete;
+		SMQueue& operator=(const SMQueue& _Other) = delete;
+		SMQueue& operator=(SMQueue&& _Other) noexcept = delete;
 
-	int Size() {
-		return QueueSize;
-	}
-
-	void Push(Type _Type) {
-		Node<Type>* NewNode = new Node(_Type);
-		++QueueSize;
-		if (nullptr == Head) {
-			Head = NewNode;
-			Tail = NewNode;
+		int Size() {
+			return QueueSize;
 		}
-		else if (Head == Tail) {
-			Head->Next = NewNode;
-			Tail = NewNode;
-		}
-		else {
-			Tail->Next = NewNode;
-			Tail = NewNode;
-		}
-	}
 
-	void Pop() {
-		--QueueSize;
-		Node<Type>* Node = Head;
-		Head = Head->Next;
-		delete Node;
-		if (0 == QueueSize) {
-			Tail = nullptr;
-			Head = nullptr;
+		void Push(Type _Type) {
+			Node<Type>* NewNode = new Node(_Type);
+			++QueueSize;
+			if (nullptr == Head) {
+				Head = NewNode;
+				Tail = NewNode;
+			}
+			else if (Head == Tail) {
+				Head->Next = NewNode;
+				Tail = NewNode;
+			}
+			else {
+				Tail->Next = NewNode;
+				Tail = NewNode;
+			}
 		}
-	}
 
-private:
-	Node<Type>* Head = nullptr;
-	Node<Type>* Tail = nullptr;
-	int QueueSize = 0;
-};
+		void Pop() {
+			--QueueSize;
+			Node<Type>* Node = Head;
+			Head = Head->Next;
+			delete Node;
+			if (0 == QueueSize) {
+				Tail = nullptr;
+				Head = nullptr;
+			}
+		}
 
+	private:
+		Node<Type>* Head = nullptr;
+		Node<Type>* Tail = nullptr;
+		int QueueSize = 0;
+	};
+
+}
