5f1c360dd748e319b7d9928e941255346ac70104
kky <kykim9050@naver.com>
2024-06-12 17:08:06 +0900

    김경윤 : 10808 문제 해결

--- assignment/KYBJVector.cpp
+++ assignment/KYBJVector.cpp
@@ -13,8 +13,22 @@ KYBJVector::~KYBJVector()
 void KYBJVector::BJ10808()
 {
 	std::string InputStr = std::string();
-
 	std::cin >> InputStr;
 
-	std::cout << InputStr << std::endl;
+	std::vector<int> Alphabets = std::vector<int>();
+
+	int ResizeVal = static_cast<int>('z' - 'a') + 1;
+
+	Alphabets.resize(ResizeVal);
+
+	for (int i = 0; i < InputStr.size(); i++)
+	{
+		++Alphabets[InputStr[i] - static_cast<int>('a')];
+	}
+
+	for (int i = 0; i < Alphabets.size(); i++)
+	{
+		std::cout << Alphabets[i];
+		std::cout << " ";
+	}
 }
