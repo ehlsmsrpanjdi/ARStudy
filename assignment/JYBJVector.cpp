5f6233d5ef28cd8802542afb3e9dd7cd63f0a17a
yoon-park <jiyoonpark.dev@gmail.com>
2024-06-17 18:14:25 +0900

    박지윤 : 빌드 에러 수정

--- assignment/JYBJVector.cpp
+++ assignment/JYBJVector.cpp
@@ -114,7 +114,7 @@ int JYBJVector::BJ1475()
 
 int JYBJVector::BJ3273()
 {
-
+	return 0;
 }
 
 int JYBJVector::BJ10807()
@@ -149,15 +149,146 @@ int JYBJVector::BJ10807()
 
 int JYBJVector::BJ13300()
 {
+	int N, K, Gender, Grade;
+	std::vector<int> GCnt(6, 0);
+	std::vector<int> BCnt(6, 0);
+	int RoomCnt = 0;
 
+	std::cin >> N >> K;
+
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> Gender >> Grade;
+
+		if (Gender == 0)
+		{
+			GCnt[Grade - 1] += 1;
+		}
+		else
+		{
+			BCnt[Grade - 1] += 1;
+		}
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (GCnt[i] != 0)
+		{
+			if (GCnt[i] % K == 0)
+			{
+				RoomCnt += GCnt[i] / K;
+			}
+			else
+			{
+				RoomCnt += (GCnt[i] / K) + 1;
+			}
+		}
+
+		if (BCnt[i] != 0)
+		{
+			if (BCnt[i] % K == 0)
+			{
+				RoomCnt += BCnt[i] / K;
+			}
+			else
+			{
+				RoomCnt += (BCnt[i] / K) + 1;
+			}
+		}
+	}
+
+	std::cout << RoomCnt;
+
+	return 0;
 }
 
 int JYBJVector::BJ11328()
 {
+	int N;
+	std::vector<std::string> Result;
 
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		std::string A, B;
+		std::vector<int> ACnt(26, 0);
+		std::vector<int> BCnt(26, 0);
+		int Index;
+		int Res = 1;
+
+		std::cin >> A >> B;
+
+		for (int i = 0; i < A.size(); i++)
+		{
+			Index = A[i] - 97;
+			ACnt[Index] += 1;
+		}
+
+		for (int i = 0; i < B.size(); i++)
+		{
+			Index = B[i] - 97;
+			BCnt[Index] += 1;
+		}
+
+		for (int i = 0; i < 26; i++)
+		{
+			if (ACnt[i] == BCnt[i])
+			{
+				Res *= 1;
+			}
+			else
+			{
+				Res *= 0;
+			}
+		}
+
+		if (Res == 1)
+		{
+			Result.push_back("Possible");
+		}
+		else
+		{
+			Result.push_back("Impossible");
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		std::cout << Result[i] << "\n";
+	}
+
+	return 0;
 }
 
 int JYBJVector::BJ1919()
 {
+	std::string A, B;
+	std::vector<int> ACnt(26, 0);
+	std::vector<int> BCnt(26, 0);
+	int Index;
+	int Res = 0;
 
+	std::cin >> A >> B;
+
+	for (int i = 0; i < A.size(); i++)
+	{
+		Index = A[i] - 97;
+		ACnt[Index] += 1;
+	}
+
+	for (int i = 0; i < B.size(); i++)
+	{
+		Index = B[i] - 97;
+		BCnt[Index] += 1;
+	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		Res += abs(ACnt[i] - BCnt[i]);
+	}
+
+	std::cout << Res;
+
+	return 0;
 }
