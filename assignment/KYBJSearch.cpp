f3f2eacbaf7b42217ff52a0811eb24e8d0f2c006
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-06-25 23:28:04 +0900

    김경윤 : BJ1926 중간 커밋

--- assignment/KYBJSearch.cpp
+++ assignment/KYBJSearch.cpp
@@ -14,36 +14,88 @@ KYBJSearch::~KYBJSearch()
 #include <vector>
 #include <queue>
 
-void KYBJSearch::BFS1926()
+void KYBJSearch::BFS1926(int _X, int _Y)
 {
+	DataCheck[_Y][_X] = true;
+	++LocalArea;
+	q.pop();
 
+	for (size_t i = 0; i < CheckDir.size(); i++)
+	{
+		int Y = _Y + CheckDir[i].y;
+		int X = _X + CheckDir[i].x;
+
+		if (Y < 0 || X < 0 || Y >= Row || X >= Col)
+		{
+			continue;
+		}
+
+		if (1 == Datas[Y][X] && false == DataCheck[Y][X])
+		{
+			DataCheck[Y][X] = true;
+			q.push({ X , Y });
+		}
+	}
+
+	while (!q.empty())
+	{
+		POINT Pos = q.front();
+
+		BFS1926(Pos.x, Pos.y);
+	}
 }
 
 void KYBJSearch::BJ1926()
 {
-	int Row = -1;
-	int Col = -1;
-	std::vector<std::vector<int>> Datas = std::vector<std::vector<int>>();
-
 	std::cin >> Row;
 	std::cin >> Col;
 
-
 	Datas.resize(Row);
+	DataCheck.resize(Row);
+
 	for (int i = 0; i < Row; i++)
 	{
 		Datas[i].resize(Col);
+		DataCheck[i].resize(Col);
 	}
 
-	int Value = -1;
+	int InputValue = -1;
 
 	for (int i = 0; i < Row; i++)
 	{
 		for (int j = 0; j < Col; j++)
 		{
-			std::cin >> Value;
-			Datas[i][j] = Value;
+			std::cin >> InputValue;
+			Datas[i][j] = InputValue;
+			DataCheck[i][j] = false;
 		}
 	}
 
+	for (int i = 0; i < Row; i++)
+	{
+		for (int j = 0; j < Col; j++)
+		{
+			if (true == DataCheck[i][j] || 0 == Datas[i][j])
+			{
+				continue;
+			}
+			
+			if (1 == Datas[i][j])
+			{
+				++PicCount;
+				q.push({ i , j });
+				BFS1926(j, i);
+
+				if (LocalArea > MaxArea)
+				{
+					MaxArea = LocalArea;
+				}
+
+				LocalArea = 0;
+			}
+		}
+	}
+
+	std::cout << PicCount << std::endl;
+	std::cout << MaxArea << std::endl;
 }
--- assignment/KYBJSearch.h
+++ assignment/KYBJSearch.h
@@ -8,12 +8,21 @@ public:
 	KYBJSearch();
 	~KYBJSearch();
 
-	void BFS1926();
+	void BFS1926(int _X, int _Y);
 	void BJ1926();
 
 protected:
 
 private:
+	int Row = -1;
+	int Col = -1;
+	std::vector<std::vector<int>> Datas = std::vector<std::vector<int>>();
+	std::vector<std::vector<bool>> DataCheck = std::vector<std::vector<bool>>();
+	std::queue<POINT> q = std::queue<POINT>();
 
+	std::vector<POINT> CheckDir = { {1, 0},{0,1},{-1,0},{0,-1} };
+
+	int PicCount = 0;
+	int LocalArea = 0;
+	int MaxArea = 0;
 };
-
